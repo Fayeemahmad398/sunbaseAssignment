45463986fc0d6260996157a9448972255f38a37b
Fayeemahmad398 <fayeemahmad0@gmail.com>
2024-02-16 12:00:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,149 @@
+import { elementTypes, formDataCollection } from "./utils.js";
+const sideBarelements = document.getElementsByClassName("types-of-elements")[0];
+let formData = formDataCollection;
+
+function renderHeader() {
+  document.getElementsByTagName("header")[0].innerHTML = `
+<div class="form-designer">Form Designer</div>
+      <button class="btn-save">Save</button>
+`;
+}
+renderHeader();
+
+function rendersidebar() {
+  sideBarelements.innerHTML = "";
+  elementTypes.forEach((ele) => {
+    sideBarelements.innerHTML += `
+        <button class="addElement" id=${ele} >
+        <span>${ele}</span> <span>+</span>
+        </button>
+        
+        `;
+  });
+}
+rendersidebar();
+
+// =============Side bar task done below==================================
+const sideBarBtns = document.querySelectorAll(".addElement");
+// tracking which Element need to Add
+sideBarBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    addThisElement(e.target.id);
+  });
+});
+
+// adding data for new Element
+function addThisElement(type) {
+  let newObjforNewElement = {
+    id: Math.random().toString(),
+    type: type,
+    label: `${type} label`,
+  };
+  if (type == "input" || type == "textarea") {
+    newObjforNewElement.placeholder = type;
+  } else {
+    newObjforNewElement.options = ["Option1", "Option2", "Option3"];
+  }
+  formData.push(newObjforNewElement);
+  RenderFormElements(formData);
+}
+
+// Rendering updated form
+let form = document.getElementsByTagName("form")[0];
+function RenderFormElements(upDatedData) {
+  form.innerHTML = "";
+  upDatedData.forEach((obj, index) => {
+    if (obj.type == "input") {
+      form.innerHTML += `
+      <div class="eachElement" id=${index}>
+      <div class="label-icons">
+        <div>${obj.label}</div>
+        <span class="material-icons deleteThis" id=${obj.id}> delete </span>
+      </div>
+      <input type="text" placeholder="${obj.placeholder}"/>
+    </div>
+
+      `;
+    } else if (obj.type == "textarea") {
+      form.innerHTML += `
+      <div class="eachElement" id=${index}>
+      <div class="label-icons">
+        <div>${obj.label}</div>
+        <span class="material-icons deleteThis" id=${obj.id}> delete </span>
+      </div>
+      <textarea name="" id="" cols="20" rows="5" placeholder="${obj.placeholder}"></textarea>
+    </div>
+
+      `;
+    } else {
+      form.innerHTML += `
+      <div class="eachElement" id=${index}>
+         <div class="label-icons">
+          <div>${obj.label}</div>
+          <span class="material-icons deleteThis" id=${obj.id}> delete </span>
+            </div>
+      <select name="">
+      <option value="" class="eachoption">Choose options</option>
+      ${obj.options.map((val) => `<option value="" class="eachoption">${val}</option>`).join()}
+        </select>
+        </div> 
+`;
+    }
+  });
+  trackDeleteIconAndDelete();
+  AttachAllEventOfDraggable();
+}
+RenderFormElements(formData);
+
+//tracking the Element to to be deleted
+function trackDeleteIconAndDelete() {
+  let deleteIconsEle = document.querySelectorAll(".deleteThis");
+  deleteIconsEle.forEach((icon) => {
+    icon.addEventListener("click", (e) => {
+      console.log(e.target.id);
+      formData = formData.filter((obj) => obj.id != e.target.id);
+      RenderFormElements(formData);
+    });
+  });
+}
+
+function AttachAllEventOfDraggable() {
+  let eachElements = document.getElementsByClassName("eachElement");
+  let arrOfEle = Array.from(eachElements);
+
+  arrOfEle.forEach((each, ind) => {
+    each.draggable = true;
+    each.addEventListener("dragstart", () => dragStart(ind));
+    each.addEventListener("dragover", (e) => dragoverIndex(e, ind));
+    each.addEventListener("drop", (e) => dropEle(e, ind));
+    // each.addEventListener("dragend", dragEnd);
+  });
+}
+
+//Consoling the json data in console(Developer tool)
+document.querySelector(".btn-save").addEventListener("click", () => {
+  console.log(formData);
+});
+
+// ================================================
+// Drag and drop functionality given below
+let dragStartInd;
+let dropInd;
+function dragStart(ind) {
+  //tracking starting index of drag
+  dragStartInd = ind;
+}
+
+function dragoverIndex(e, ind) {
+  //tracking gragOver  index
+  e.preventDefault();
+}
+
+function dropEle(e, ind) {
+  // updating order of Elements
+  e.preventDefault();
+  dropInd = ind;
+  let deltedEle = formData.splice(dragStartInd, 1);
+  formData.splice(dropInd, 0, deltedEle[0]);
+  RenderFormElements(formData);
+}
---
+++ utils.js
@@ -0,0 +1,28 @@
+export let formDataCollection = [
+  {
+    id: "c0ac49c5-871e-4c72-a878-251de465e6b4",
+    type: "input",
+    label: "input Label",
+    placeholder: "input  placeholder",
+  },
+  {
+    id: "146e69c2-1630-4a27-9d0b-f09e463a66e4",
+    type: "select",
+    label: "Select Label",
+    options: ["Option1", "Option2", "Option3"],
+  },
+  {
+    id: "45002ecf-85cf-4852-bc46-529f94a758f5",
+    type: "input",
+    label: "Input Label",
+    placeholder: "input Placeholder",
+  },
+  {
+    id: "680cff8d-c7f9-40be-8767-e3d6ba420952",
+    type: "textarea",
+    label: "textarea Label",
+    placeholder: "textarea Placeholder",
+  },
+];
+
+export const elementTypes = ["input", "select", "textarea"];
